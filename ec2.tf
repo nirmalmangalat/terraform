88355ca8ae446ada831d84a2f61414ff44fcb9f8
nirmalmangalat <76103125+nirmalmangalat@users.noreply.github.com>
2020-12-16 20:31:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
 resource "aws_instance" "myawsserver" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.micro"
+  key_name = "NIrmal-Key"
 
   tags = {
-    Name = "NirmalVM100"
-    Env = "Prod"
-    client = "Mangalat"
+    Name = "NIrmaldevops"
+    env = "test"
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> /root/ansilist"
   }
 }
-
-output "myawsserver-ip" {
-  value = "${aws_instance.myawsserver.public_ip}"
-}
-
